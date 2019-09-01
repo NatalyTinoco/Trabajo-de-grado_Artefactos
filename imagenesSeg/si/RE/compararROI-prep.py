c01490c95d56aa6eaa0946d849dfbfba46fe4037
ddiazpal1 <ddiazpal@unbosque.edu.co>
2019-09-01 17:39:13 -0500

    idgf

--- imagenesSeg/si/RE/compararROI-prep.py
+++ imagenesSeg/si/RE/compararROI-prep.py
@@ -4,12 +4,14 @@ Created on Mon Jul 15 23:27:13 2019
 
 @author: Nataly
 """
+
 import sys
 sys.path.insert(1,'C:/Users/Usuario/Documents/Daniela/Tesis/Trabajo-de-grado_Artefactos/funciones')
 
 from skimage.measure import compare_ssim as ssim
+from sklearn.cluster import KMeans
+from sklearn.metrics import pairwise_distances_argmin_min
 import cv2
-import xlrd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import distance
@@ -36,113 +38,121 @@ def dice(im1, im2):
     return Dice    
 import glob
 
-#similitud=np.zeros((307,16))
-#mSSIM=np.zeros((307,16))
-#mMSE=np.zeros((307,16))
-#mDICE1=np.zeros((307,16))
-#mDICE2=np.zeros((307,16))
 mSSIM=[],[],[],[],[],[],[],[],[]
 mMSE=[],[],[],[],[],[],[],[],[]
 mDICE2=[],[],[],[],[],[],[],[],[]
 fileTT=[]
-read=[]
+area=[]
+perimetro=[]
 i=0
 
-workbook = xlrd.open_workbook("segentadasSI.xlsx")
-sheet_names = workbook.sheet_names()
-xl_sheet = workbook.sheet_by_name(sheet_names[0])
+for image in glob.glob('*.jpg'):   
+    kmeansAr=[[],[],[],[],[],[]]
+    print(image)
+    img2 = cv2.imread(image,0)
+    imrgb = cv2.imread(image)
+    copyNorm = img2.copy()
+    
+    imgeFile = image.split(".jpg")
+    original2 = cv2.imread('C:/Users/Usuario/Documents/Daniela/Tesis/Trabajo-de-grado_Artefactos/imagenesSeg/si/RE/'+imgeFile[0]+".tif",0)
+    
+#    imNorm = cv2.normalize(img2,None,0,1,norm_type=cv2.NORM_MINMAX,dtype=cv2.CV_8UC3)
+    contours,hierarchy = cv2.findContours(img2, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    for c in range(len(contours)):
+#        print(c)
+        cnt = contours[c]
+        x,y,w,h = cv2.boundingRect(cnt)
+        epsilon = 0.01*cv2.arcLength(cnt,True)
+        approx = cv2.approxPolyDP(cnt,epsilon,True)
+        #cv2.drawContours(original, [approx],-1, (0,255,0), 3)
+        cv2.rectangle(imrgb,(x,y),(x+w,y+h),(0,255,0),2)
+        
+        area.append(cv2.contourArea(approx))
+        perimetro.append(cv2.arcLength(approx,True))
+        
+        kmeansAr[0].append(x)
+        kmeansAr[1].append(y)
+        kmeansAr[2].append(w)
+        kmeansAr[3].append(h)
+        kmeansAr[4].append(cv2.contourArea(approx))
+        kmeansAr[5].append(cv2.arcLength(approx,True))
+        
+    pp = cv2.cvtColor(imrgb, cv2.COLOR_BGR2RGB)
+    plt.imshow(pp)
+    plt.show()  
+#    dire='./kmeans-Seg/'+imgeFile[0]+'-contorno.jpg'
+#    cv2.imwrite(dire,imrgb)
+    
+    X=np.array(list(zip(kmeansAr[4],kmeansAr[5])))
+    kmeans = KMeans(n_clusters=2)
+    kmeans = kmeans.fit(X)
+    labels = kmeans.predict(X)
+    centroids = kmeans.cluster_centers_
+    
+    colors=["m.","r.",".c","y.","b."]
+    for i in range(len(X)):
+        print('Coordenada: ',X[i],'Etiqueta: ',labels[i])
+        plt.plot(X[i][0],X[i][1],colors[labels[i]],markersize=10)
+    plt.scatter(centroids[:,0],centroids[:,1],marker='*',s=150,linewidths=5,zorder=10)
+    plt.show()
+    
+    labels2 = labels.tolist()
+    grupo0 = [i for i,x in enumerate(labels2) if x == 0]
+    grupo1 = [i for i,x in enumerate(labels2) if x == 1]
+    
+    valores =0
+    valores2 =0
+    for p in grupo0:
+        valores = kmeansAr[4][p] + valores
+    valores = valores/len(grupo0)
+    
+    for q in grupo1:
+        valores2 = kmeansAr[4][q] + valores2
+    valores2 = valores2/len(grupo1)
+    
+    if valores2 < valores:
+        for z in grupo0:
+            labels2[z]=1
+        for w in grupo1:
+            labels2[w]=0
+            
+    for m in [i for i,x in enumerate(labels2) if x == 1]:
+        if kmeansAr[4][m] > 2500 and kmeansAr[4][m] > 200:
+            print(kmeansAr[4][m])
+            copyNorm[kmeansAr[1][m]:kmeansAr[1][m]+kmeansAr[3][m], kmeansAr[0][m]:kmeansAr[0][m]+kmeansAr[2][m]] = 0
+    
+    pp = cv2.cvtColor(copyNorm, cv2.COLOR_BGR2RGB)
+    plt.imshow(pp)
+    
+#    dire='./kmeans-Seg/'+ image
+#    cv2.imwrite(dire,copyNorm)
+            
+#            if cv2.contourArea(approx) > 4000 and cv2.arcLength(approx,True) > 300:
+#                 x,y,w,h = cv2.boundingRect(cnt)
+#                 f,c= copyNorm[y:y+h, x:x+w].shape
+#                 copyNorm[y:y+h, x:x+w] = 0
+#                 pp = cv2.cvtColor(copyNorm, cv2.COLOR_BGR2RGB)
+#                 plt.imshow(pp)
 
-for col in range(xl_sheet.ncols):
-    ttfile = xl_sheet.cell_value(1, col)
-    if ttfile != '':
-        ttfile=ttfile+'tif'
-        fileTT.append(ttfile)
+ 
+    print(ssim(original2, original2))
+    print(mse(original2, original2))
+    print('dice2',dice(original2, original2))
+    tabla=[original2,copyNorm]
+    for h in range(len(tabla)):
+        mSSIM[h].append(ssim(original2, tabla[h]))
+        mMSE[h].append(mse(original2, tabla[h]))  
+        mDICE2[h].append(dice(original2, tabla[h]))          
 
-for image in glob.glob('*.tif'):    
-    if image not in fileTT:
-        read.append(image)
-        image = '00103.tif'
-        print(image)
-        original = cv2.imread(image,0)
-        image2 = image.split(".")
-        imageFile = image2[0]+'.jpg'
-        tecnica1= umbralAdapArt(imageFile)  
-        tecnica2 = sthele2006(imageFile)
-        tecnica3 = ventanIDEA(imageFile)
-        
-        dire='./Segmentadas_Prepro/15/T1/'+imageFile
-        cv2.imwrite(dire,tecnica1)
-        dire='./Segmentadas_Prepro/15/T2/'+imageFile
-        cv2.imwrite(dire,tecnica2)
-        dire='./Segmentadas_Prepro/15/T3/'+imageFile
-        cv2.imwrite(dire,tecnica3)
-        
-#        segventanas=cv2.imread('C:/Users/Usuario/Documents/Daniela/Tesis/Trabajo-de-grado_Artefactos/subData/segmentacionSthele_CanalG_ventanas/RE/'+image2[0]+'.jpg',0)
-#        segtoda=cv2.imread('C:/Users/Usuario/Documents/Daniela/Tesis/Trabajo-de-grado_Artefactos/subData/segmentacionSthele_CanalG_ConEcuAdaptativa/RE/'+image2[0]+'.jpg',0)
-#        segumbral=cv2.imread('C:/Users/Usuario/Documents/Daniela/Tesis/Trabajo-de-grado_Artefactos/subData/segmentacionRE_Articulo/RE/'+image2[0]+'.jpg',0)
-#        segventanas_2=cv2.imread('C:/Users/Usuario/Documents/Daniela/Tesis/Trabajo-de-grado_Artefactos/subData/segmentacionSthele_CanalG_ventanas/RE_2/'+image2[0]+'.jpg',0)
-#        segventanas_3=cv2.imread('C:/Users/Usuario/Documents/Daniela/Tesis/Trabajo-de-grado_Artefactos/subData/segmentacionSthele_CanalG_ventanas/RE_3/'+image2[0]+'.jpg',0)
-#        segventanas_4=cv2.imread('C:/Users/Usuario/Documents/Daniela/Tesis/Trabajo-de-grado_Artefactos/subData/segmentacionSthele_CanalG_ventanas/RE_4/'+image2[0]+'.jpg',0)
-#        segventanas_5=cv2.imread('C:/Users/Usuario/Documents/Daniela/Tesis/Trabajo-de-grado_Artefactos/subData/segmentacionSthele_CanalG_ventanas/RE_5/'+image2[0]+'.jpg',0)
-#        segventanas_6=cv2.imread('C:/Users/Usuario/Documents/Daniela/Tesis/Trabajo-de-grado_Artefactos/subData/segmentacionSthele_CanalG_ventanas/RE_6/'+image2[0]+'.jpg',0)
-        print(ssim(original, original))
-        print(mse(original, original))
-        print('dice2',dice(original, original))
-        tabla=[original,tecnica1,tecnica2,tecnica3]
-        for h in range(len(tabla)):
-            mSSIM[h].append(ssim(original, tabla[h]))
-            mMSE[h].append(mse(original, tabla[h]))  
-            mDICE2[h].append(dice(original, tabla[h]))          
-        i=i+1
-#%%
-        
-for col in range(xl_sheet.ncols):
-    ttfile = xl_sheet.cell_value(0, col)
-    if ttfile != '':
-        ttfile=ttfile+'tif'
-        if ttfile not in read:
-            print(ttfile)
 #%%
 import pandas as pd    
-#datos = {'SSIM_Original':mSSIM[0],
-#         'MSE_Original':mMSE[0],
-#         'DICE_Original':mDICE2[0],
-#         'SSIM_Articulo':mSSIM[1],
-#         'MSE_Articulo':mMSE[1],
-#         'DICE_Articulo':mDICE2[1],
-#         'SSIM_ImgComp':mSSIM[2],
-#         'MSE_ImgComp':mMSE[2],
-#         'DICE_ImgComp':mDICE2[2],
-#         'SSIM_ImgVen':mSSIM[3],
-#         'MSE_ImgVen':mMSE[3],
-#         'DICE_ImgVen':mDICE2[3],
-#         'SSIM_ImgVen2':mSSIM[4],
-#         'MSE_ImgVen2':mMSE[4],
-#         'DICE_ImgVen2':mDICE2[4],
-#         'SSIM_ImgVen3':mSSIM[5],
-#         'MSE_ImgVen3':mMSE[5],
-#         'DICE_ImgVen3':mDICE2[5],
-#         'SSIM_ImgVen4':mSSIM[6],
-#         'MSE_ImgVen4':mMSE[6],
-#         'DICE_ImgVen4':mDICE2[6],
-#         'SSIM_ImgVen5':mSSIM[7],
-#         'MSE_ImgVen5':mMSE[7],
-#         'DICE_ImgVen5':mDICE2[7],
-#         'SSIM_ImgVen6':mSSIM[8],
-#         'MSE_ImgVen6':mMSE[8],
-#         'DICE_ImgVen6':mDICE2[8]}
+
 
 datos = {'SSIM_Original':mSSIM[0],
          'MSE_Original':mMSE[0],
          'DICE_Original':mDICE2[0],
-         'SSIM_T1':mSSIM[1],
-         'MSE_T1':mMSE[1],
-         'DICE_T1':mDICE2[1],
-         'SSIM_T2':mSSIM[2],
-         'MSE_T2':mMSE[2],
-         'DICE_T2':mDICE2[2],
-         'SSIM_T3':mSSIM[3],
-         'MSE_T3':mMSE[3],
-         'DICE_T3':mDICE2[3]}
+         'SSIM_T3':mSSIM[1],
+         'MSE_T3':mMSE[1],
+         'DICE_T3':mDICE2[1],}
 datos = pd.DataFrame(datos)
-datos.to_excel('medidasSimilitudRE-prepro15.xlsx') 
+datos.to_excel('medidasSimilitudRE-kmeans2.xlsx') 
