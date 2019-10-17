28d883123ace4a1a3a088c2b463357e0d35abda8
NatalyTinoco <51056570+NatalyTinoco@users.noreply.github.com>
2019-10-17 02:04:45 -0500

    YACASIIIIiiiiii

--- funciones/test_todoRE.py
+++ funciones/test_todoRE.py
@@ -45,25 +45,36 @@ def test_all_RE(imagePath):
    
     _,contours,_ = cv2.findContours(umbrImage,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
     
-    for c in range(len(contours)):
-        cnt = contours[c]
-#        epsilon = 0.01*cv2.arcLength(cnt,True)
-#        approx = cv2.approxPolyDP(cnt,epsilon,True)
-        x,y,w,h = cv2.boundingRect(cnt)
-        
-#        cropped1 = imDU[int(y):int(y+h),int(x):int(x+w)]
-        cropped2 = imDR[int(y):int(y+h),int(x):int(x+w)]
-        
-        brillo,contraste,desvi=caracRe(cropped2)
-        carac=pd.DataFrame({'contrastB':contraste,'desviacionB':desvi,'Brillo':brillo},index =['1'])
-        pred=int(mpRE.predict(carac))
-        print(pred)
-        
-        if pred == 1:
-            umbrImage[int(y):int(y+h),int(x):int(x+w)] = umbrImage[int(y):int(y+h),int(x):int(x+w)]
-        else:
-            umbrImage[int(y):int(y+h),int(x):int(x+w)] = 0
+#    umbrImage_1 = cv2.normalize(umbrImage, None, 0, 255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC3)
+    
+
+    if len(contours)==0:
+         pred=0
+    else:
+        for c in range(len(contours)):
+         
+            cnt = contours[c]
+    #        epsilon = 0.01*cv2.arcLength(cnt,True)
+    #        approx = cv2.approxPolyDP(cnt,epsilon,True)
+            x,y,w,h = cv2.boundingRect(cnt)
             
+    #        cropped1 = imDU[int(y):int(y+h),int(x):int(x+w)]
+            cropped2 = imDR[int(y):int(y+h),int(x):int(x+w)]
+            
+            brillo,contraste,desvi=caracRe(cropped2)
+            carac=pd.DataFrame({'contrastB':contraste,'desviacionB':desvi,'Brillo':brillo},index =['1'])
+            pred=int(mpRE.predict(carac))
+            if pred == 1:
+                umbrImage[int(y):int(y+h),int(x):int(x+w)] = umbrImage[int(y):int(y+h),int(x):int(x+w)]
+            else:
+                umbrImage[int(y):int(y+h),int(x):int(x+w)] = 0
+            
+#            umbrImage_2 = cv2.normalize(umbrImage, None, 0, 255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC3)
+#        
+#            cv2.imshow('imageres',umbrImage_2)
+#            cv2.waitKey(0)
+#            cv2.destroyAllWindows()
+        
     return pred, original_2 , imDU_2,umbrImage
 #    cv2.imwrite('contorno.jpg',original)
 #    cv2.imwrite('C:/Users/Usuario/Documents/Daniela/Tesis/Trabajo-de-grado_Artefactos/test-todo/'+filePath+'/'+str(c)+'-contorno.jpg',cropped)
